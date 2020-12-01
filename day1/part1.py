3d9f0b21e79464fa0206fee149da9409f50cbc1e
cbrooker-27 <34251497+cbrooker-27@users.noreply.github.com>
2020-12-01 18:06:44 -0500

    add comments

--- day1/part1.py
+++ day1/part1.py
@@ -1,10 +1,8 @@
-#python
-
-with open('input.txt') as f:
-    lines = f.read().splitlines()
-    for x in range(len(lines)):
-        for y in range(x+1,len(lines)):
-            sum = int(lines[x])+int(lines[y])
-            if sum == 2020 :
-                product = int(lines[x])*int(lines[y])
-                print("x={} y={} {}+{}={} and product = {}".format(x,y,lines[x],lines[y],sum,product))
+with open('input.txt') as f: #read input into file 'f'
+    lines = f.read().splitlines() # split lines from file 'f' into 'lines'
+    for x in range(len(lines)): # use x to iterate through lines
+        for y in range(x+1,len(lines)): # use y to also iterate through lines
+            sum = int(lines[x])+int(lines[y]) # figure out the sum of the numbers on line x and y
+            if sum == 2020 : # if the sum is 2020
+                product = int(lines[x])*int(lines[y]) #find the product of the magic numbers
+                print("x={} y={} {}+{}={} and product = {}".format(x,y,lines[x],lines[y],sum,product)) #print the values of x and y, sum and product

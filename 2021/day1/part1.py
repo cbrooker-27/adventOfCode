ffdc2f78be00c0c2d7e11d05d7c4e85e123d2652
Chris Brooker <cbrooke@us.ibm.com>
2021-12-04 19:50:31 -0500

    part one complete

--- 2021/day1/part1.py
+++ 2021/day1/part1.py
@@ -1,8 +1,8 @@
+increasingCount=0
 with open('input.txt') as f: #read input into file 'f'
     lines = f.read().splitlines() # split lines from file 'f' into 'lines'
-    for x in range(len(lines)): # use x to iterate through lines
-        for y in range(x+1,len(lines)): # use y to also iterate through lines
-            sum = int(lines[x])+int(lines[y]) # figure out the sum of the numbers on line x and y
-            if sum == 2020 : # if the sum is 2020
-                product = int(lines[x])*int(lines[y]) #find the product of the magic numbers
-                print("x={} y={} {}+{}={} and product = {}".format(x,y,lines[x],lines[y],sum,product)) #print the values of x and y, sum and product
+    for x in range(len(lines)-1): # use x to iterate through lines
+        if int(lines[x+1])>int(lines[x]):
+            increasingCount = increasingCount+1
+            print("when line {} is greater than line {}".format(x+1,x))
+print("{} increasing counts found".format(increasingCount))

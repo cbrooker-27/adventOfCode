03ce320893f9a0bf340eb0b55efe0fa6648d8b1d
Chris Brooker <cbrooke@us.ibm.com>
2021-12-04 20:00:53 -0500

    added an alternate solution for part2

--- 2021/day1/part2 alt.py
+++ 2021/day1/part2 alt.py
@@ -1,12 +1,9 @@
 increasingCount=0
-window1=0
-window2=0
 with open('input.txt') as f: #read input into file 'f'
     lines = f.read().splitlines() # split lines from file 'f' into 'lines'
     for x in range(len(lines)-3): # use x to iterate through lines
-        window1 = int(lines[x])+int(lines[x+1])+int(lines[x+2])
-        window2 = int(lines[x+1])+int(lines[x+2])+int(lines[x+3])
-        if window2>window1:
+        #since only the first and last numbers are different (the 2 in the middle are shared), we can just compare the two numbers
+        if int(lines[x+3])>int(lines[x]):
             increasingCount = increasingCount+1
             print("when line is {} next sum is greater".format(x))
 print("{} increasing counts found".format(increasingCount))

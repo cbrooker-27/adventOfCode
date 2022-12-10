f0c458453a4efdc0b0eee53290284fc9b59843ff
Chris Brooker <cbrooker27@gmail.com>
2022-12-09 21:41:30 -0500

    fix error in sample day

--- 2022/sampleday/part1.py
+++ 2022/sampleday/part1.py
@@ -1,3 +1,4 @@
 with open('input.txt') as f: #read input into file 'f'
     lines = f.read().splitlines() # split lines from file 'f' into 'lines'
     for x in range(len(lines)): # use x to iterate through lines
+        print('hey')

8fb7a0c77d81337f53838357367b5059182232cc
Chris Brooker <cbrooker27@gmail.com>
2022-12-09 20:34:38 -0500

    update sample day

---
+++ 2022/sampleday/part2.py
@@ -0,0 +1,3 @@
+with open('input.txt') as f: #read input into file 'f'
+    lines = f.read().splitlines() # split lines from file 'f' into 'lines'
+    for x in range(len(lines)): # use x to iterate through lines
